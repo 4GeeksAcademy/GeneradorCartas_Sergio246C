d61b6a2db9cc031e84ba149ba115275f2115c2a0
Sergio Cordoba <sergiocordoba246@gmail.com>
2025-11-11 18:40:51 +0000

    Falta el boton

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-import "bootstrap";
-import "./style.css";
+import './style.css';
+window.onload = function () {
+  const palos = ["♦", "♥", "♠", "♣"]
+  const num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
 
+  const generarCartas = () => {
+    const randomPalos = palos[Math.floor(Math.random() * palos.length)];
+    const randomNum = num[Math.floor(Math.random() * num.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    const isRed = randomPalos === "♥" || randomPalos === "♦";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    document.querySelector(".top-suit").innerHTML = randomPalos
+    document.querySelector(".number").innerHTML = randomNum
+    document.querySelector(".bottom-suit").innerHTML = randomPalos
+    if (isRed) {
+      document.querySelector(".card").classList.add("red")
+    }
+
+  }
 };
+
+
+
