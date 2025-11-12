dfe2810e38bcf6d90fb9ca11caca90f65f9ee9fc
Sergio Cordoba <sergiocordoba246@gmail.com>
2025-11-12 10:50:32 +0000

    Entrega final

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 import './style.css';
-window.onload = function () {
-  const palos = ["♦", "♥", "♠", "♣"]
-  const num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
+const palos = ["♦", "♥", "♠", "♣"];
+const num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
 
-  const generarCartas = () => {
-    const randomPalos = palos[Math.floor(Math.random() * palos.length)];
-    const randomNum = num[Math.floor(Math.random() * num.length)];
+const generarCartas = () => {
+  const randomPalos = palos[Math.floor(Math.random() * palos.length)];
+  const randomNum = num[Math.floor(Math.random() * num.length)];
 
-    const isRed = randomPalos === "♥" || randomPalos === "♦";
-
-    document.querySelector(".top-suit").innerHTML = randomPalos
-    document.querySelector(".number").innerHTML = randomNum
-    document.querySelector(".bottom-suit").innerHTML = randomPalos
-    if (isRed) {
-      document.querySelector(".card").classList.add("red")
-    }
-
+  if (randomPalos === "♥" || randomPalos === "♦") {
+    document.querySelector(".card").classList.add("cardRed")
   }
+
+  document.querySelector(".top-suit").innerHTML = randomPalos
+  document.querySelector(".number").innerHTML = randomNum
+  document.querySelector(".bottom-suit").innerHTML = randomPalos
+}
+
+window.onload = function () {
+  generarCartas()
 };
 
 
